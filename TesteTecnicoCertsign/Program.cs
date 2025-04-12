09501aa10486e0bc2c5bd2073c0dd150c9527266
Arthur-Sena <asassena1002@gmail.com>
2025-04-11 21:41:20 -0300

    Teste tecnico certisign

---
+++ TesteTecnicoCertsign/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> listNum = [-2, 1, 3, 5, 7, 12];
+Dictionary<int, int> possiveisResultados = [];
+int result = 1;
+
+for (int i = 0; i< listNum.Count(); i++)
+{
+    bool resultadoEncontrado = possiveisResultados.TryGetValue(listNum[i], out int valor);
+    if (listNum[i] == result)
+    {
+        Console.WriteLine($"{i}");
+        return;
+    }
+    else if (resultadoEncontrado)
+    {
+        Console.WriteLine($"{i} {valor}");
+        return;
+    }
+
+    possiveisResultados.Add(result - listNum[i], i);
+}
